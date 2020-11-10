152667c6a6bd4b389ac51a62f933acc18a4e2d88
fady.moukawem <fady.moukawam.f@gmail.com>
2020-11-10 16:37:22 +0200

    Test Components added

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,13 @@
 /*!
-
 =========================================================
 * Material Dashboard React - v1.9.0
 =========================================================
-
 * Product Page: https://www.creative-tim.com/product/material-dashboard-react
 * Copyright 2020 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
 * Coded by Creative Tim
-
 =========================================================
-
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 */
 // @material-ui/icons
 import Dashboard from "@material-ui/icons/Dashboard";
@@ -22,7 +16,7 @@ import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import BubbleChart from "@material-ui/icons/BubbleChart";
 import LocationOn from "@material-ui/icons/LocationOn";
 import Notifications from "@material-ui/icons/Notifications";
-import Unarchive from "@material-ui/icons/Unarchive";
+// import Unarchive from "@material-ui/icons/Unarchive";
 import Language from "@material-ui/icons/Language";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
@@ -32,9 +26,10 @@ import Typography from "views/Typography/Typography.js";
 import Icons from "views/Icons/Icons.js";
 import Maps from "views/Maps/Maps.js";
 import NotificationsPage from "views/Notifications/Notifications.js";
-import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.js";
+// import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.js";
 // core components/views for RTL layout
 import RTLPage from "views/RTLPage/RTLPage.js";
+import TestComponents from "views/TestComponents/TestComponents";
 
 const dashboardRoutes = [
   {
@@ -94,6 +89,14 @@ const dashboardRoutes = [
     layout: "/admin"
   },
   {
+    path: "/testfady",
+    name: "Test Components",
+    rtlName: "تجربة",
+    icon: Notifications,
+    component: TestComponents,
+    layout: "/admin"
+  },
+  {
     path: "/rtl-page",
     name: "RTL Support",
     rtlName: "پشتیبانی از راست به چپ",
@@ -101,14 +104,6 @@ const dashboardRoutes = [
     component: RTLPage,
     layout: "/rtl"
   },
-  // {
-  //   path: "/upgrade-to-pro",
-  //   name: "Upgrade To PRO",
-  //   rtlName: "التطور للاحترافية",
-  //   icon: Unarchive,
-  //   component: UpgradeToPro,
-  //   layout: "/admin"
-  // }
 ];
 
 export default dashboardRoutes;
